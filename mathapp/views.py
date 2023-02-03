7d35debc496ddbf6e93704491be98842a9837d4e
Mankret <asilvergrave@gmail.com>
2023-02-03 11:53:21 +0200

    hotfix

--- mathapp/views.py
+++ mathapp/views.py
@@ -6,12 +6,13 @@ from mathapp.forms import HypotenuseForm
 
 
 def triangle(request):
-    form = HypotenuseForm()
-    if request.GET:
-        form = HypotenuseForm(request.GET)
+
+    form = HypotenuseForm(request.GET)
+    if form.is_valid():
         f = request.GET.get('f_cath')
         s = request.GET.get('s_cath')
         hyp = math.sqrt(int(f)**2 + int(s)**2)
         return render(request, 'hypotenuse.html', {'hyp': hyp, 'form': form})
     else:
+        form = HypotenuseForm()
         return render(request, 'hypotenuse.html', {'form': form})
